90cf6ff99b74fe3e44e11b05d192dc3b8d819b65
aleeteo <aleteo946@gmail.com>
2025-08-04 19:51:34 +0200

    get centroids is more efficient now

--- code/get_metadata.py
+++ code/get_metadata.py
@@ -1,21 +1,11 @@
 import json
+import numpy as np
 
 
-def get_centroids(meta: dict) -> list:
-    centroids = []
-
-    for mcc, coords in meta["MCCCoord"].items():
-        # Separiamo le coordinate x e y
-        xs = [p[0] for p in coords]
-        ys = [p[1] for p in coords]
-
-        # Calcoliamo il centroide come media aritmetica
-        cx = sum(xs) / len(xs)
-        cy = sum(ys) / len(ys)
-
-        centroids.append((cx, cy))
-
-    return centroids
+def get_centroids(meta: dict) -> np.ndarray:
+    mcc_coords = meta["MCCCoord"]
+    # Convertiamo ogni lista di coordinate in un array numpy e calcoliamo la media lungo l'asse 0 (x e y)
+    return np.array([np.mean(coords, axis=0) for coords in mcc_coords.values()])
 
 
 if __name__ == "__main__":
@@ -25,5 +15,5 @@ if __name__ == "__main__":
         data = json.load(f)
     meta = data["Place0"]
 
-    gen = get_centroids(meta)
-    print("Centroids:", gen)
+    centroids = get_centroids(meta)
+    print("Centroids:\n", centroids)
