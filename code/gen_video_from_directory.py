94159e094b870e7575ce45fd72778f8fa171c06b
aleeteo <aleteo946@gmail.com>
2025-08-08 16:19:08 +0200

    ora vengono cancatenati i video da un centroide all'altro per creare un video "triangolare"

--- code/gen_video_from_directory.py
+++ code/gen_video_from_directory.py
@@ -27,7 +27,8 @@ def gen_video_from_directory(input_dir: str) -> list:
     with open(meta_path, "r", encoding="utf-8") as f:
         meta_data = json.load(f)
 
-    # -------------------------------------------------------
+    results = []
+
     for place in meta_data.keys():
         # -------------------------------------------------------
         # estrazione del nome dell'immagine e della GT
@@ -44,49 +45,75 @@ def gen_video_from_directory(input_dir: str) -> list:
         img = u.imread(tiff_path, depth)
         gt = u.imread(gt_path, depth)
 
+        # TODO: trasformare tutta sta roba in parametri
+        #
         # impostazione del seed per la riproducibilità e altri parametri del video
         seed = random.randint(0, 10000)
         zoom = 3
-        fps = 15
-        video_duration = 3  # durata del video in secondi
+        nframes = 15
+        # TODO: applicare le maschere ai video
+
         # recupero i centroidi dei colorChecker
         centroids = get_centroids(meta_data[place])
+        n = len(centroids)
+        if n < 2:
+            # niente da fare: un solo centroide non consente transizioni
+            continue
 
-        # TODO: applicare le maschere ai video
-        # TODO: concatenare i video in un unico video
+        # coppie consecutive con wrap-around: (0->1, 1->2, ..., n-1->0)
+        pairs = [(i, (i + 1) % n) for i in range(n)]
 
-        video_frames = artificial_video.ken_burns_advanced(
-            img,
-            start_point=centroids[0],
-            end_point=centroids[1],
-            start_zoom=zoom,
-            end_zoom=zoom,
-            interp_pan="linear",
-            interp_zoom="ease_in_out",
-            noise_strength=0.6,
-            noise_speed=0.2,
-            noise_seed=seed,
-            video_fps=fps,
-            video_duration=video_duration,
+        # genera segmenti e accumula
+        segments = []
+        gt_segments = []
+
+        for a, b in pairs:
+            seg = artificial_video.ken_burns_advanced(
+                img,
+                start_point=centroids[a],
+                end_point=centroids[b],
+                start_zoom=zoom,
+                end_zoom=zoom,
+                interp_pan="linear",
+                interp_zoom="ease_in_out",
+                noise_strength=0.9,
+                noise_speed=0.2,
+                noise_seed=seed,
+                num_frames=nframes,
+            )
+            segments.append(seg)
+
+            gt_seg = artificial_video.ken_burns_advanced(
+                gt,
+                start_point=centroids[a],
+                end_point=centroids[b],
+                start_zoom=zoom,
+                end_zoom=zoom,
+                interp_pan="linear",
+                interp_zoom="ease_in_out",
+                noise_strength=0.9,
+                noise_speed=0.2,
+                noise_seed=seed,
+                num_frames=nframes,
+            )
+            gt_segments.append(gt_seg)
+
+        # concatena tutti i segmenti in un unico video (asse temporale)
+        video_frames = np.concatenate(segments, axis=0)
+        gt_video_frames = np.concatenate(gt_segments, axis=0)
+
+        # playback (facoltativo)
+        artificial_video.play_video_from_array(video_frames, fps=20)
+        artificial_video.play_video_from_array(gt_video_frames, fps=20)
+
+        # salva nel risultato
+        results.append(
+            {
+                "key": place,
+                "video": video_frames,
+                "gt_video": gt_video_frames,
+            }
         )
-
-        gt_video_frames = artificial_video.ken_burns_advanced(
-            gt,
-            start_point=centroids[0],
-            end_point=centroids[1],
-            start_zoom=zoom,
-            end_zoom=zoom,
-            interp_pan="linear",
-            interp_zoom="ease_in_out",
-            noise_strength=0.5,
-            noise_speed=0.2,
-            noise_seed=seed,
-            video_fps=fps,
-            video_duration=video_duration,
-        )
-
-        artificial_video.play_video_from_array(video_frames, fps=fps)
-        artificial_video.play_video_from_array(gt_video_frames, fps=fps)
 
     return []
 
