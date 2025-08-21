6499429c07662e768e027af66a667a6be6fd985a
aleeteo <aleteo946@gmail.com>
2025-08-21 11:44:03 +0200

    aggiunta funzione per restituire le coppie di centroidi

--- code/get_metadata.py
+++ code/get_metadata.py
@@ -1,11 +1,22 @@
 import json
 import numpy as np
+from typing import Tuple
 
 
 def get_centroids(meta: dict) -> np.ndarray:
     mcc_coords = meta["MCCCoord"]
     # Calcolo della media e divisione per 2
     return np.array([np.mean(coords, axis=0) for coords in mcc_coords.values()]) / 2
+
+
+def get_centroids_pairs(meta: dict) -> Tuple[list, np.ndarray]:
+    """
+    Restituisce una lista di coppie di centri dei colorChecker.
+    """
+    centroids = get_centroids(meta)
+    n = len(centroids)
+    pairs = [(i, (i + 1) % n) for i in range(n)]
+    return (pairs, centroids)
 
 
 if __name__ == "__main__":
